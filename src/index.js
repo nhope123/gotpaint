5d9559cc705de2b0813a52971220f4d6095b13f9
nhope <16958187+nhope123@users.noreply.github.com>
2020-11-28 12:30:13 -0500

    bug:  initrd.img error

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import store from './store/store.js';
+import {store, persistor} from './store/store.js';
+import { PersistGate } from 'redux-persist/integration/react'
 import './style/index.scss';
 import './style/nav.scss';
 import './style/form.scss';
@@ -9,11 +10,14 @@ import './style/inventory.scss';
 //import App from './App';
 import Paint from './components/paintApp.js';
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <Paint />
+      <PersistGate loading={null} persistor={persistor}>
+        <Paint />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
@@ -22,4 +26,5 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+serviceWorker.register();
 reportWebVitals();
